5d0937c51f3b7f7f921797f3c8019c9d46b67e91
Maxime521 <9842@holbertonstudents.com>
2024-11-29 14:35:53 +0000

    Create the files print_i_d.c and the input the code task 1

---
+++ print_i_d.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * print_i_d - prints an integer
+ * @args: list of arguments
+ *
+ * Return: int - as length
+ */
+
+int print_i_d(va_list args)
+{
+	int arg = va_arg(args, int);
+	int pow = 1, len = 0;
+	unsigned int tmp_num;
+
+	if (arg < 0)
+	{
+		_putchar('-');
+		arg *= -1;
+		len++;
+	}
+
+	tmp_num = arg;
+
+	if (tmp_num == 0)
+	{
+		_putchar("0");
+		return (len + 1);
+	}
+
+	while (tmp_num / pow >= 10)
+		pow *= 10;
+
+	while (pow > 0)
+	{
+		_putchar((tmp_num / pow) + '0');
+		tmp_num %= pow;
+		pow /= 10;
+		len++;
+	}
+
+	return (len);
+}
