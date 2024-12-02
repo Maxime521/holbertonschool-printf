dbdfc29c4d9d2ddb1e1eb0c5bdbb054767843c9b
root <root@ElodieSeck.localdomain>
2024-12-02 17:21:02 +0100

    the principal file

--- _printf.c
+++ _printf.c
@@ -1,52 +1,43 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stddef.h>
+
 /**
- * _printf - Produces formatted output.
- * @format: Format string containing the text and format specifiers.
+ * _printf - Function that produce output according to a format.
  *
- * Return: Total number of characters printed.
+ * @format: String that can include text to print or format specifiers, if any.
+ *
+ * Return: The length of the printed string &/or printed args, -1 if !format.
  */
+
 int _printf(const char *format, ...)
 {
-	int i = 0, len = 0;
-	int j;
+	int i = 0, total_lengh = 0;
 	va_list args;
+
+	va_start(args, format);
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	va_start(args, format);
 
-	while (format && format[i])
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+
+	while (format[i])
 	{
-		if (format[i] == '%' && format[i + 1])
+		if (format[i] == '%')
 		{
-			i++;
-			if (format[i] == 'c')
-				len += _putchar(va_arg(args, int));
-			else if (format[i] == 's')
-			{
-				char *str = va_arg(args, char *);
-
-				if (!str)
-					str = "(null)";
-				for (j = 0; str[j]; j++, len++)
-					_putchar(str[j]);
-			}
-			else if (format[i] == '%')
-				len += _putchar('%');
-			else
-			{
-				len += _putchar('%');
-				len += _putchar(format[i]);
-			}
+			total_lengh += get_specifier(format[i + 1], args);
+			i += 2;
 		}
+
 		else
 		{
-			len += _putchar(format[i]);
+			total_lengh++;
+			_putchar(format[i]);
+			i++;
 		}
-		i++;
 	}
+
 	va_end(args);
-	return (len);
+
+	return (total_lengh);
 }
